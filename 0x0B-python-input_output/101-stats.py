5419a4674a7e55dca05caf3cb935e42cb1b56cf3
saraiin <saraennaciri96@gmail.com>
2023-07-11 15:56:38 -0700

    stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -54,6 +54,6 @@ if __name__ == "__main__":
 
         print_stats(size, status_codes)
 
-    except KeyboardInterrupt:
+    except KeyboardInterrupt as err:
         print_stats(size, status_codes)
         raise
