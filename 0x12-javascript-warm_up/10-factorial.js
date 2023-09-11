ddba18c35151407ceb064ee856fffa299dfb8b4d
saraiin <saraennaciri96@gmail.com>
2023-09-11 16:00:13 -0700

    fix var

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 function factorial (num) {
   if (isNaN(num) || num === 0) return 1;
-  return num * factorial(n - 1);
+  return num * factorial(num - 1);
 }
 
 console.log(factorial(Number(process.argv[2])));
