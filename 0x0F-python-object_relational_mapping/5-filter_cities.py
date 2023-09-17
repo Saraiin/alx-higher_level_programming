3bc132ca33113ab0ccb739086c6825e75d202897
saraiin <saraennaciri96@gmail.com>
2023-09-17 13:05:55 -0700

    fix code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,11 +9,18 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
+    HOST = "localhost"
+    PORT = 3306
+    USER = argv[1]
+    PASS = argv[2]
+    DB = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(host=HOST,
+                         port=PORT,
+                         user=USER,
+                         passwd=PASS,
+                         db=DB,
                          charset="utf8")
 
     cursor = db.cursor()
