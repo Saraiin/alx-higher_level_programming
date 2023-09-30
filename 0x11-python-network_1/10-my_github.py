3fa83229b630afc82031b620da3e896746b8093f
saraiin <saraennaciri96@gmail.com>
2023-09-29 17:04:13 -0700

    tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
+"""Python script that:
+    * takes your GitHub credentials (username and password)
+    * uses the GitHub API to display your id
+    * must use Basic Authentication with a personal access
+    token as password to access to your information
+    (only read:user permission is needed)
+    * The first argument will be your username
+    * The second argument will be your password
 """
-import sys
 import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    url = "https://api.github.com/user"
+    basic_auth = HTTPBasicAuth(argv[1], argv[2])
+    resp = requests.get(url, auth=basic_auth)
+    print(resp.json().get("id"))
