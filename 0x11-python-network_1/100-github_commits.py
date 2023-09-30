f7d4682bc7bc180308062553d4544b599fd6eb85
saraiin <saraennaciri96@gmail.com>
2023-09-29 17:00:25 -0700

    tasks

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://developer.github.com/repos/v3/repos/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
