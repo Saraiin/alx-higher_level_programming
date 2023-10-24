9d30dbdf99a23cd92bf87f6d6a003514fa73c6c6
saraiin <saraennaciri96@gmail.com>
2023-10-24 11:45:53 -0700

    update

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+let url = 'http://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
+~  
