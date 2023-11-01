58873f821b6a56be76d1863676b62daa8d416156
saraiin <saraennaciri96@gmail.com>
2023-11-01 15:14:30 -0700

    update

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+const $ = window.$;
+$(document).ready(function () {
+  const helloEl = $('DIV#hello');
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+  $.get(url, function (data) {
+    helloEl.text(data.hello);
+  });
+});
